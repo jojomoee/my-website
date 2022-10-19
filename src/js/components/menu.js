2acceac28adb9c24d15939133c1129030a732386
Johannes Moe <johannesmoehrl@protonmail.com>
2022-10-19 02:50:30 +0200

    menu animation playing around

--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -8,15 +8,32 @@ export function animationCloseOpen() {
 
   const tl = gsap.timeline();
 
+  tl.set('#menu-background', {
+    scale: 0.2,
+  });
+
   tl.to('#menu-background', 0.4, {
     visibility: 'visible',
     rotation: 180,
-	scale: 4,  
+    transformOrigin: '50% 50%',
     ease: 'back.out(1.3)',
+    scale: 0.9,
   });
+
+  tl.to('.path1', 0.4, {
+    fill: 'red',
+  });
+
   tl.reversed(true);
 
   function backgroundAnimation() {
     tl.reversed(!tl.reversed());
   }
+
+  const tl2 = gsap.timeline();
+
+  tl2.to('#menu', 100, {
+    rotation: 999,
+    transformOrigin: '50% 50%',
+  });
 }
--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,9 @@ import './../scss/style.scss';
 import gsap from 'gsap';
 import { animationCloseOpen } from './components/menu';
 
-animationCloseOpen();
+document.addEventListener('DOMContentLoaded', function (event) {
+  animationCloseOpen();
+});
 
 //<button id="counter" type="button"></button>
 //setupCounter(document.querySelector('#counter'));
